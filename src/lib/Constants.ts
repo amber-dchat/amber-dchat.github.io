3083e4895ceed6419d1a387f5fc11b24e2be71b6
AHQ Miness <ahqsecret@gmail.com>
2024-09-27 15:50:13 +0530

    revert timeout to 10 sec

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,3 +1,3 @@
-export const TIMEOUT_DELAY = 3_000 as const;
+export const TIMEOUT_DELAY = 10_000 as const;
 export const formatDataStores = (d: string, type: 'chat') =>
 	`dchatt-app-platform/${type}/${d}`;
