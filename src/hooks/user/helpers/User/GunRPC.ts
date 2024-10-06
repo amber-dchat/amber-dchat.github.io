dc3874bb601c2ed20a411fa6380fe1ed1597d854
Retro <70205403+retrouser955@users.noreply.github.com>
2024-10-06 16:26:20 +0630

    init: GunRPC

---
+++ src/hooks/user/helpers/User/GunRPC.ts
@@ -0,0 +1,75 @@
+import { GunUserInstance } from "../../useMainUser";
+
+export const GUN_RPC_USER_KEY = "rpcstate" as const;
+
+export type RPCImage = {
+    url: string;
+    hover?: string;
+}
+
+export type RPCType = "playing" | "listening" | "watching";
+
+export type RPCButtons = {
+    text: string;
+    url: string;
+}
+
+export type RPCRaw = {
+    name: string;
+    hasImage: boolean;
+    imageMainURL?: string;
+    imageMainHover?: string;
+    imageSubURL?: string;
+    imageSubHover?: string;
+    sub: string;
+    hasButtons: boolean;
+    buttonPrimaryText?: string;
+    buttonPrimaryURL?: string;
+    buttonSecondaryText?: string;
+    buttonSecondaryURL?: string;
+    hasStamps: boolean;
+    stampStart?: number;
+    stampEnd?: number;
+}
+
+export type RPC = {
+    name: string;
+    images?: {
+        main: RPCImage;
+        sub?: RPCImage;
+    }
+    sub: string;
+    buttons?: {
+        primary: RPCButtons;
+        secondary?: RPCButtons;
+    }
+    timestamp?: {
+        start: Date;
+        end?: Date;
+    }
+}
+
+// PLEASE: Clean before exit app
+export class UserRPCManager {
+    state: RPC | null = null;
+    private user: GunUserInstance
+
+    constructor(user: GunUserInstance, state?: Promise<RPC> | RPC) {
+        this.user = user;
+        if(state) {
+            if(state instanceof Promise) {
+                (async () => {
+                    this.state = await state;
+                })();
+            } else {
+                this.state = state;
+            }
+        }
+    }
+
+    set(state: RPC | null) {
+        if(state === null) {
+            this.user.get(GUN_RPC_USER_KEY).put(null)
+        }
+    }
+}
